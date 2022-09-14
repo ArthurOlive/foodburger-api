5cfe71c45cc670cddeff5955b0dce1a9656068a4
Arthur Vitor <arthuroliveira909.av@gmail.com>
2022-09-14 20:56:35 -0300

    first.

---
+++ src/main/java/com/api/foodburg/FoodburgApplication.java
@@ -0,0 +1,13 @@
+package com.api.foodburg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodburgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodburgApplication.class, args);
+	}
+
+}
